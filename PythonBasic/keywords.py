a4b59c34c8e57c2547efc0a03bb32d18f7656cae
NimnaKs <nimnakaveesha123456@gmail.com>
2024-03-04 15:06:33 +0530

    keywords of python

---
+++ PythonBasic/keywords.py
@@ -0,0 +1,28 @@
+'''
+In Python, keywords are reserved words that
+have a special meaning and cannot be used as
+identifiers (names for variables, functions,
+classes, etc.).
+
+These keywords define the
+syntax and structure of the Python language.
+Here is the list of Python keywords:
+
+False      class      finally    is         return
+None       continue   for        lambda     try
+True       def        from       nonlocal   while
+and        del        global     not        with
+as         elif       if         or         yield
+assert     else       import     pass
+break      except     in         raise
+
+
+These keywords are case-sensitive, so `True` and `true` would be treated differently.
+
+'''
+
+import keyword
+
+keyword_list = keyword.kwlist
+print(keyword_list)
+
