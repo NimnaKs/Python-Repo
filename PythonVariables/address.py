ac830946a7d53837e30cac3188af3a6c2537436c
NimnaKs <nimnakaveesha123456@gmail.com>
2024-03-04 20:45:34 +0530

    Python Variable Adress

---
+++ PythonVariables/address.py
@@ -0,0 +1,8 @@
+a = 5
+print(f"a : {a}")
+print(f"Address of a : {id(a)}")
+print(f"Hexadecimal of Address of a : "
+      f"{hex(id(a))}")
+
+# id() -> Function to get address of a variable
+# hex() -> Function to convert value to hexadecimal
