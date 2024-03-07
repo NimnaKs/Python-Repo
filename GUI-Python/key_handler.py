3769ed6efe77abec8c15b67721f206b593c8ab8f
NimnaKs <nimnakaveesha123456@gmail.com>
2024-03-07 19:37:00 +0530

    Key Handler Done

---
+++ GUI-Python/key_handler.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+def key_handler(event):
+    print(f"char:{event.char} "
+          f"sym:{event.keysym} "
+          f"code:{event.keycode} ")
+
+
+window.bind("<Key>",key_handler)
+
+window.mainloop()
