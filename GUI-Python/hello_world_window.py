90aed0a18e1c742d56924b830b177bf4341df5ef
NimnaKs <nimnakaveesha123456@gmail.com>
2024-03-07 14:56:02 +0530

    create hello world window done

---
+++ GUI-Python/hello_world_window.py
@@ -0,0 +1,13 @@
+import tkinter as frame
+
+window = frame.Tk()
+
+message = frame.Label(text="Hello World")
+
+message.pack()
+
+print("Start main loop")
+
+window.mainloop()
+
+print("End main loop")
