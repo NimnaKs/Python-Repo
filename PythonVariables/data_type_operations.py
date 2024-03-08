b79f28bf68ee92d7c8ea527972d3d2297bae9984
NimnaKs <nimnakaveesha123456@gmail.com>
2024-03-08 22:49:48 +0530

    Data type Operation Done

---
+++ PythonVariables/data_type_operations.py
@@ -0,0 +1,26 @@
+x, y = -43, 22
+
+print(f"x+y : {x+y}")
+print(f"x-y : {x-y}")
+print(f"x*y : {x*y}")
+print(f"x/y : {x/y}")
+
+print(f"x//y : {x//y}")
+# Floored to nearest 0 position
+
+print(f"x%y : {x%y}")
+print(f"-x : {-x}")
+print(f"+x : {+x}")
+print(f"abs(x) : {abs(x)}")
+print(f"int(x) : {int(x)}")
+print(f"float(x) : {float(x)}")
+
+print(f"divmod(x,y) : {divmod(x,y)}")
+# Return the pair (x//y,x%y)
+
+print(f"pow(x,y) : {pow(x,y)}")
+# X to power Y
+print(f"x**y : {x**y}")
+# X to power Y
+
+
