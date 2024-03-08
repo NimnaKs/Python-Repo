75f50735461daf93c2f7092c63f83c6e9df1bd6a
NimnaKs <nimnakaveesha123456@gmail.com>
2024-03-08 23:04:23 +0530

    String Done

---
+++ PythonVariables/Strings.py
@@ -0,0 +1,35 @@
+# Creating Strings
+
+# 01.Single quotes
+var_str1 = 'Hello World !'
+
+# 02.Double quotes
+var_str2 = "Hello World !"
+
+# 03.Triple quotes
+var_str2 = '''Hello World !'''
+
+# String constructor
+string_int = str(5)
+string_float = str(5.5)
+
+"""
+name = "Sahan"
+name[1] = "P"
+Traceback (most recent call last):
+  File "/Users/mac/Desktop/Python/PythonVariables/mutable_vs_immutable.py", line 16, in <module>
+    name[1] = "P"
+TypeError: 'str' object does not support item assignment
+
+del var_str1[0]
+Traceback (most recent call last):
+  File "/Users/mac/Desktop/Python/PythonVariables/Strings.py", line 16, in <module>
+    del var_str1[0]
+TypeError: 'str' object doesn't support item deletion
+
+"""
+
+# String Iterator
+
+print(var_str1)
+
