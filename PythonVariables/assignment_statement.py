18e7f0aa23f012d6620495fec9739bd229ae21af
NimnaKs <nimnakaveesha123456@gmail.com>
2024-03-08 22:17:53 +0530

    Assignment Statement Done

---
+++ PythonVariables/assignment_statement.py
@@ -0,0 +1,10 @@
+x = 10
+print(f"Address bind to X : {id(x)}")
+
+x = x + 1
+print(f"Address bind to X : {id(x)}")
+
+my_list_1 = ["Nimna", "Kaveesha", "Sekara"]
+print(f"Address bind to my_list_1 : {id(x)}")
+my_list_2 = ["Nimna", "Kaveesha", "Sekara"]
+print(f"Address bind to my_list_2 : {id(x)}")
